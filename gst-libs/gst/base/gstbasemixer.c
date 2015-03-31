fd067e7824d371fa0c3b21e7d156dce94d53a9b6
Thiago Santos <thiagoss@osg.samsung.com>
2015-03-31 10:04:45 -0300

    basemixer: new base class for mixers
    Subclass of aggregator that does time alignment of buffers before
    handing them to the real implementation just for the mixing.
    
    Subclasses can implement the 'mix' function instead of the aggregate.
    The implementation should just iterate over the pads, get the buffers
    and mix them, generating an output buffer to be passed to the
    gst_aggregator_finish_buffer function

---
+++ gst-libs/gst/base/gstbasemixer.c
@@ -0,0 +1,281 @@
+/* GStreamer base_mixer base class
+ * Copyright (C) 2015 Samsung Electronics. All rights reserved.
+ *   Author: Thiago Santos <thiagoss@osg.samsung.com>
+ *
+ * gstbasemixer.c:
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+/**
+ * SECTION: gstbasemixer
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#include "gstbasemixer.h"
+
+
+GST_DEBUG_CATEGORY_STATIC (base_mixer_debug);
+#define GST_CAT_DEFAULT base_mixer_debug
+
+struct _GstBaseMixerPadPrivate
+{
+  GstClockTime offset;
+};
+
+/***********************************
+ * GstBaseMixerPad implementation  *
+ ************************************/
+G_DEFINE_TYPE (GstBaseMixerPad, gst_base_mixer_pad, GST_TYPE_AGGREGATOR_PAD);
+
+static void
+gst_base_mixer_pad_class_init (GstBaseMixerPadClass * klass)
+{
+  g_type_class_add_private (klass, sizeof (GstBaseMixerPadPrivate));
+}
+
+static void
+gst_base_mixer_pad_init (GstBaseMixerPad * pad)
+{
+  pad->priv =
+      G_TYPE_INSTANCE_GET_PRIVATE (pad, GST_TYPE_BASE_MIXER_PAD,
+      GstBaseMixerPadPrivate);
+}
+
+static void
+gst_base_mixer_pad_advance (GstBaseMixerPad * mixerpad, GstClockTime duration)
+{
+  GstBuffer *buffer;
+
+  GST_LOG_OBJECT (mixerpad, "Advancing: %" GST_TIME_FORMAT,
+      GST_TIME_ARGS (duration));
+
+  buffer = gst_aggregator_pad_get_buffer (GST_AGGREGATOR_PAD_CAST (mixerpad));
+  if (buffer) {
+    mixerpad->priv->offset += duration;
+    if (GST_BUFFER_DURATION_IS_VALID (buffer)) {
+
+      g_assert (GST_BUFFER_DURATION (buffer) >= mixerpad->priv->offset);
+
+      /* It should never be smaller but let's leave it here for safety */
+      if (GST_BUFFER_DURATION (buffer) <= mixerpad->priv->offset) {
+        gst_aggregator_pad_drop_buffer (GST_AGGREGATOR_PAD_CAST (mixerpad));
+        mixerpad->priv->offset = 0;
+      }
+    }
+    gst_buffer_unref (buffer);
+  }
+}
+
+/*************************************
+ * GstBaseMixer implementation  *
+ *************************************/
+static GstElementClass *base_mixer_parent_class = NULL;
+
+/* All members are protected by the object lock unless otherwise noted */
+
+struct _GstBaseMixerPrivate
+{
+  gint non_empty;
+};
+
+typedef struct _GstBaseMixerTimeAlignment
+{
+  GstClockTime start;
+  GstClockTime end;
+} GstBaseMixerTimeAlignment;
+
+static gboolean
+gst_base_mixer_find_time_alignment_foreach (GstAggregator * agg,
+    GstAggregatorPad * aggpad, gpointer udata)
+{
+  GstBaseMixerPad *mixerpad = GST_BASE_MIXER_PAD_CAST (aggpad);
+  GstBaseMixerTimeAlignment *talign = udata;
+  GstBuffer *buf;
+
+  /* TODO handle GST_CLOCK_TIME_NONE */
+  /* TODO check if segment/buffer in aggregator is racy */
+  /* TODO do we need to lock the pad here? */
+  /* TODO do we need to translate the timestamps here to the output segment?
+   *      or is it always good as it is based on running time? */
+
+  GST_LOG_OBJECT (aggpad,
+      "Checking pad data. Current offset: %" GST_TIME_FORMAT,
+      GST_TIME_ARGS (mixerpad->priv->offset));
+
+  buf = gst_aggregator_pad_get_buffer (aggpad);
+  if (buf) {
+    GstClockTime start = GST_CLOCK_TIME_NONE, end = GST_CLOCK_TIME_NONE;
+    if (GST_BUFFER_PTS_IS_VALID (buf)) {
+      start =
+          gst_segment_to_running_time (&aggpad->segment, GST_FORMAT_TIME,
+          GST_BUFFER_PTS (buf) + mixerpad->priv->offset);
+      if (GST_BUFFER_DURATION_IS_VALID (buf)) {
+        end = start + GST_BUFFER_DURATION (buf) - mixerpad->priv->offset;       /* TODO clip on segment */
+
+        if (end < talign->end)
+          talign->end = end;
+      }
+
+      if (start < talign->start)
+        talign->start = start;
+    }
+    GST_DEBUG_OBJECT (aggpad,
+        "Pad has times: %" GST_TIME_FORMAT " - %" GST_TIME_FORMAT,
+        GST_TIME_ARGS (start), GST_TIME_ARGS (end));
+    gst_buffer_unref (buf);
+  }
+
+  return TRUE;
+}
+
+static void
+gst_base_mixer_find_time_alignment (GstBaseMixer * bmixer, GstClockTime * start,
+    GstClockTime * end)
+{
+  GstBaseMixerTimeAlignment timealign;
+
+  timealign.start = GST_CLOCK_TIME_NONE;
+  timealign.end = GST_CLOCK_TIME_NONE;
+
+  GST_DEBUG_OBJECT (bmixer, "Starting time alignment");
+
+  gst_aggregator_iterate_sinkpads (GST_AGGREGATOR_CAST (bmixer),
+      gst_base_mixer_find_time_alignment_foreach, &timealign);
+  *start = timealign.start;
+  *end = timealign.end;
+
+  GST_DEBUG_OBJECT (bmixer,
+      "Time aligned data to: %" GST_TIME_FORMAT " - %" GST_TIME_FORMAT,
+      GST_TIME_ARGS (*start), GST_TIME_ARGS (*end));
+}
+
+static gboolean
+gst_base_mixer_advance_foreach (GstAggregator * agg, GstAggregatorPad * aggpad,
+    gpointer udata)
+{
+  GstBaseMixerPad *mixerpad = GST_BASE_MIXER_PAD_CAST (aggpad);
+  GstClockTime *duration = udata;
+
+  gst_base_mixer_pad_advance (mixerpad, *duration);
+  return TRUE;
+}
+
+static void
+gst_base_mixer_advance (GstBaseMixer * bmixer, GstClockTime duration)
+{
+  gst_aggregator_iterate_sinkpads (GST_AGGREGATOR_CAST (bmixer),
+      gst_base_mixer_advance_foreach, &duration);
+}
+
+GstFlowReturn
+gst_base_mixer_finish_buffer (GstBaseMixer * bmixer, GstBuffer * buffer)
+{
+  GstFlowReturn ret;
+
+  ret = gst_aggregator_finish_buffer (GST_AGGREGATOR_CAST (bmixer), buffer);
+  gst_base_mixer_advance (bmixer, GST_BUFFER_DURATION (buffer));
+
+  return ret;
+}
+
+static GstFlowReturn
+gst_base_mixer_mix (GstBaseMixer * bmixer, GstClockTime start, GstClockTime end)
+{
+  GstBaseMixerClass *bmixer_class = GST_BASE_MIXER_GET_CLASS (bmixer);
+
+  g_return_val_if_fail (bmixer_class->mix != NULL, GST_FLOW_ERROR);
+
+  return bmixer_class->mix (bmixer, start, end);
+}
+
+static GstFlowReturn
+gst_base_mixer_aggregate (GstAggregator * agg, gboolean timeout)
+{
+  GstBaseMixer *bmixer = GST_BASE_MIXER_CAST (agg);
+  GstClockTime start, end;
+  GstFlowReturn ret;
+
+  gst_base_mixer_find_time_alignment (bmixer, &start, &end);
+
+  ret = gst_base_mixer_mix (bmixer, start, end);
+
+  return ret;
+}
+
+static void
+gst_base_mixer_finalize (GObject * object)
+{
+  G_OBJECT_CLASS (base_mixer_parent_class)->finalize (object);
+}
+
+/* GObject vmethods implementations */
+static void
+gst_base_mixer_class_init (GstBaseMixerClass * klass)
+{
+  GObjectClass *gobject_class = (GObjectClass *) klass;
+  GstAggregatorClass *aggregator_class = (GstAggregatorClass *) klass;
+
+  base_mixer_parent_class = g_type_class_peek_parent (klass);
+  g_type_class_add_private (klass, sizeof (GstBaseMixerPrivate));
+
+  GST_DEBUG_CATEGORY_INIT (base_mixer_debug, "base_mixer",
+      GST_DEBUG_FG_MAGENTA, "GstBaseMixer");
+
+  gobject_class->finalize = gst_base_mixer_finalize;
+
+  aggregator_class->aggregate = gst_base_mixer_aggregate;
+
+  aggregator_class->sinkpads_type = GST_TYPE_BASE_MIXER_PAD;
+}
+
+static void
+gst_base_mixer_init (GstBaseMixer * self, GstBaseMixerClass * klass)
+{
+  self->priv =
+      G_TYPE_INSTANCE_GET_PRIVATE (self, GST_TYPE_BASE_MIXER,
+      GstBaseMixerPrivate);
+}
+
+/* we can't use G_DEFINE_ABSTRACT_TYPE because we need the klass in the _init
+ * method to get to the padtemplates */
+GType
+gst_base_mixer_get_type (void)
+{
+  static volatile gsize type = 0;
+
+  if (g_once_init_enter (&type)) {
+    GType _type;
+    static const GTypeInfo info = {
+      sizeof (GstBaseMixerClass),
+      NULL,
+      NULL,
+      (GClassInitFunc) gst_base_mixer_class_init,
+      NULL,
+      NULL,
+      sizeof (GstBaseMixer),
+      0,
+      (GInstanceInitFunc) gst_base_mixer_init,
+    };
+
+    _type = g_type_register_static (GST_TYPE_AGGREGATOR,
+        "GstBaseMixer", &info, G_TYPE_FLAG_ABSTRACT);
+    g_once_init_leave (&type, _type);
+  }
+  return type;
+}
---
+++ gst-libs/gst/base/gstbasemixer.h
@@ -0,0 +1,157 @@
+/* GStreamer base mixer class
+ * Copyright (C) 2015 Samsung Electronics. All rights reserved.
+ *   Author: Thiago Santos <thiagoss@osg.samsung.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GST_BASE_MIXER_H__
+#define __GST_BASE_MIXER_H__
+
+#ifndef GST_USE_UNSTABLE_API
+#warning "The Base library from gst-plugins-bad is unstable API and may change in future."
+#warning "You can define GST_USE_UNSTABLE_API to avoid this warning."
+#endif
+
+#include <gst/gst.h>
+#include "gstaggregator.h"
+
+G_BEGIN_DECLS
+
+/**************************
+ * GstBaseMixer Structs  *
+ *************************/
+
+typedef struct _GstBaseMixer GstBaseMixer;
+typedef struct _GstBaseMixerPrivate GstBaseMixerPrivate;
+typedef struct _GstBaseMixerClass GstBaseMixerClass;
+
+/************************
+ * GstBaseMixerPad API *
+ ***********************/
+
+#define GST_TYPE_BASE_MIXER_PAD            (gst_base_mixer_pad_get_type())
+#define GST_BASE_MIXER_PAD(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_BASE_MIXER_PAD, GstBaseMixerPad))
+#define GST_BASE_MIXER_PAD_CAST(obj)       ((GstBaseMixerPad *)(obj))
+#define GST_BASE_MIXER_PAD_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_BASE_MIXER_PAD, GstBaseMixerPadClass))
+#define GST_BASE_MIXER_PAD_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),GST_TYPE_BASE_MIXER_PAD, GstBaseMixerPadClass))
+#define GST_IS_BASE_MIXER_PAD(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_BASE_MIXER_PAD))
+#define GST_IS_BASE_MIXER_PAD_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_BASE_MIXER_PAD))
+
+/****************************
+ * GstBaseMixerPad Structs *
+ ***************************/
+
+typedef struct _GstBaseMixerPad GstBaseMixerPad;
+typedef struct _GstBaseMixerPadClass GstBaseMixerPadClass;
+typedef struct _GstBaseMixerPadPrivate GstBaseMixerPadPrivate;
+
+/**
+ * GstBaseMixerPad:
+ * @buffer: currently queued buffer.
+ * @segment: last segment received.
+ *
+ * The implementation of the GstPad to use with #GstBaseMixer
+ */
+struct _GstBaseMixerPad
+{
+  GstAggregatorPad            parent;
+
+  /* < Private > */
+  GstBaseMixerPadPrivate   *  priv;
+
+  gpointer _gst_reserved[GST_PADDING];
+};
+
+/**
+ * GstBaseMixerPadClass:
+ * @flush:    Optional
+ *            Called when the pad has received a flush stop, this is the place
+ *            to flush any information specific to the pad, it allows for individual
+ *            pads to be flushed while others might not be.
+ *
+ */
+struct _GstBaseMixerPadClass
+{
+  GstAggregatorPadClass   parent_class;
+
+  GstFlowReturn (*flush)  (GstBaseMixerPad * bmpad, GstBaseMixer * bmixer);
+
+  /*< private >*/
+  gpointer      _gst_reserved[GST_PADDING_LARGE];
+};
+
+GType gst_base_mixer_pad_get_type           (void);
+
+/****************************
+ * GstBaseMixerPad methods *
+ ***************************/
+
+/*********************
+ * GstBaseMixer API *
+ ********************/
+
+#define GST_TYPE_BASE_MIXER            (gst_base_mixer_get_type())
+#define GST_BASE_MIXER(obj)            (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_BASE_MIXER,GstBaseMixer))
+#define GST_BASE_MIXER_CAST(obj)       ((GstBaseMixer *)(obj))
+#define GST_BASE_MIXER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_BASE_MIXER,GstBaseMixerClass))
+#define GST_BASE_MIXER_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj),GST_TYPE_BASE_MIXER,GstBaseMixerClass))
+#define GST_IS_BASE_MIXER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_BASE_MIXER))
+#define GST_IS_BASE_MIXER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_BASE_MIXER))
+
+#define GST_FLOW_NOT_HANDLED           GST_FLOW_CUSTOM_SUCCESS
+
+/**
+ * GstBaseMixer:
+ * @srcpad: the base mixer's source pad
+ *
+ * BaseMixer base class object structure.
+ */
+struct _GstBaseMixer
+{
+  GstAggregator            parent;
+
+  GstPad                *  srcpad;
+
+  /*< private >*/
+  GstBaseMixerPrivate  *  priv;
+
+  gpointer                 _gst_reserved[GST_PADDING_LARGE];
+};
+
+/**
+ * GstBaseMixerClass:
+ */
+struct _GstBaseMixerClass {
+  GstAggregatorClass   parent_class;
+
+  GstFlowReturn (*mix) (GstBaseMixer * bmixer, GstClockTime start, GstClockTime end);
+
+  /*< private >*/
+  gpointer          _gst_reserved[GST_PADDING_LARGE];
+};
+
+/*************************
+ * GstBaseMixer methods *
+ ************************/
+
+GType gst_base_mixer_get_type(void);
+
+GstFlowReturn gst_base_mixer_finish_buffer (GstBaseMixer * bmixer, GstBuffer * buffer);
+
+G_END_DECLS
+
+#endif /* __GST_BASE_MIXER_H__ */
